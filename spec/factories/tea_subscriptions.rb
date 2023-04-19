332bab6d43a243448f9115347132da14046109ca
Braxton Eddings <beddings81@gmail.com>
2023-04-19 11:13:39 -0500

    Remove tea_subscription table

--- spec/factories/tea_subscriptions.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :tea_subscription do
-    association :subscription, :tea
-  end
-end
--- spec/models/tea_subscription_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe TeaSubscription do
-  describe 'associations' do
-    it { should belong_to(:tea)}
-    it { should belong_to(:subscription)}
-  end
-end
