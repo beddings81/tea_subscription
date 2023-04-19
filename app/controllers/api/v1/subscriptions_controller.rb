f9f05669fa9ef222a4651c9f0527475a4449cc95
Braxton Eddings <beddings81@gmail.com>
2023-04-19 14:18:24 -0500

    Complete update subscription endpoint

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -4,6 +4,16 @@ class Api::V1::SubscriptionsController < ApplicationController
     render json: SubscriptionSerializer.new(subscription), status: 201
   end
 
+  def update
+    subscription = Subscription.find(params[:id])
+    if subscription.status == 'active'
+      subscription.update!(status: 'cancelled')
+    else
+      subscription.update!(status: 'active')
+    end
+    render json: SubscriptionSerializer.new(subscription)
+  end
+
   private
 
   def subscription_params
